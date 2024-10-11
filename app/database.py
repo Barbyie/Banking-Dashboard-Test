675a3e4293f14cf46b9946b6e09533e49d3780dc
Thiago Barbieri Catarina <tcatarina@proton.me>
2024-10-11 19:52:43 -0300

    Remove unnecessary comments

--- app/database.py
+++ app/database.py
@@ -2,19 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLite database URL
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app/bank.db"
 
-# Create the SQLite engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-# Create a base class for models
 Base = declarative_base()
 
-# Create a sessionmaker
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
